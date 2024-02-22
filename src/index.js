3c618932023de9aa2a95857ea519e8341f2e3d0e
yaragattipraveen <yaragattipraveen68@gmail.com>
2024-02-22 19:11:22 +0530

    worked on  models

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ connectDB().then(
   app.listen(process.env.PORT || 8000, () => {
     console.log(`The server is running on PORT: ${process.env.PORT || 8000}`)
   })
+  
 ).catch((error) => {
   console.log("MongoDB connection failed", error)
 })
